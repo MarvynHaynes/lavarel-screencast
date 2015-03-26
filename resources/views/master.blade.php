9e259fed93800ccbaa1027b32dce354cec58bd81
haynesm2 <marvyn.haynes@gmail.com>
2015-03-26 01:39:00 -0400

    First upload of files, part 4 completed

---
+++ resources/views/master.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+	@yield('content')
+
+	@yield('footer')
+</body>
+</html>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+
+@section('content')
+	About Me
+
+@stop
+
+
+@section('footer')
+<script>
+	alert('about page here');
+</script>
+
+@stop
